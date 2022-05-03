a5ceb994885494064213bcea3bd789f3343912d0
zanewolf <zwolf.mlxvi@gmail.com>
2022-05-02 21:55:37 -0400

    updated netlify schedule function

--- netlify/functions/automate_build_function.js
+++ netlify/functions/automate_build_function.js
@@ -1,11 +1,16 @@
 const { schedule } = require('@netlify/functions')
+const fetch = require('node-fetch');
+
+const REBUILD_URL = process.env.GATSBY_NETLIFY_BUILD_HOOK;
 
 const handler = async function(event, context) {
     console.log("Received event:", event)
+
+    await fetch(REBUILD_URL, { method: 'POST'});
 
     return {
         statusCode: 200,
     };
 };
 
-module.exports.handler = schedule("@hourly", handler);
+module.exports.handler = schedule("@daily", handler);
