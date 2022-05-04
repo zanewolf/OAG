132ce27b78143ecb6833b0d53f94e7089c2003c8
zanewolf <zwolf.mlxvi@gmail.com>
2022-05-03 20:02:30 -0400

    added try...catch to rebuild function and changed to hourly to troubleshoot

--- netlify/functions/automate_build_function.js
+++ netlify/functions/automate_build_function.js
@@ -6,11 +6,18 @@ const REBUILD_URL = process.env.GATSBY_NETLIFY_BUILD_HOOK;
 const handler = async function(event, context) {
     console.log("Received event:", event)
 
-    await fetch(REBUILD_URL, { method: 'POST'});
+    try{
+        let response = await fetch(REBUILD_URL, { method: 'POST'});
+        if (!response.ok){
+            throw new Error(response.statusText);
+        }
+    } catch (err){
+        console.log('automated_build_function error: ', err)
+    }
 
     return {
         statusCode: 200,
     };
 };
 
-module.exports.handler = schedule("@daily", handler);
+module.exports.handler = schedule("@hourly", handler);
