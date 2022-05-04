a821e63b36b443f74069edc08bf7b1b5fce6c9a0
zanewolf <zwolf.mlxvi@gmail.com>
2022-05-03 23:19:19 -0400

    trying rebuild with API call not build hook

--- netlify/functions/automate_build_function.js
+++ netlify/functions/automate_build_function.js
@@ -1,26 +1,62 @@
-const { schedule } = require('@netlify/functions')
-const fetch = require('node-fetch');
+//
+// rebuild-redirects.js
+// - Scheduled function to automatically rebuild the site at a set schedule
+// - Schedule is defined in the `netlify.toml` in the repository root though
+//   it is possible to define it in this function. For more see
+//   https://github.com/netlify/labs/tree/main/features/scheduled-functions/documentation
+// - Time is set in the project `netlify.toml`
+//
+//
+// SPDX-License-Identifier: Jam
+//
 
-const REBUILD_URL = process.env.GATSBY_NETLIFY_BUILD_HOOK;
-
-// absolute vs relative urls?
-// build hook?
+import fetch from "node-fetch";
 
 const handler = async function(event, context) {
-    console.log("Received event:", event)
 
-    try{
-        let response = await fetch(REBUILD_URL, { method: 'POST'});
-        if (!response.ok){
-            throw new Error(response.statusText);
+    // Trigger a rebuild
+    const postUrl = `https://api.netlify.com/api/v1/sites/${process.env.GATSBY_NETLIFY_SITE_ID}/builds`;
+
+    const response = await fetch(postUrl, {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${process.env.GATSBY_NETLIFY_OAUTH_ID}`,
+            "User-Agent": `${process.env.GATSBY_NETLIFY_UA_TOKEN}`
         }
-    } catch (err){
-        console.log('automated_build_function error: ', err)
-    }
+    });
+
+    const result = await response.json();
+    console.log("Result: ", result);
 
     return {
         statusCode: 200,
     };
 };
 
-module.exports.handler = schedule("@hourly", handler);
+module.exports.handler = handler;
+// const { schedule } = require('@netlify/functions')
+// const fetch = require('node-fetch');
+//
+// const REBUILD_URL = process.env.GATSBY_NETLIFY_BUILD_HOOK;
+//
+// // absolute vs relative urls?
+// // build hook?
+//
+// const handler = async function(event, context) {
+//     console.log("Received event:", event)
+//
+//     try{
+//         let response = await fetch(REBUILD_URL, { method: 'POST'});
+//         if (!response.ok){
+//             throw new Error(response.statusText);
+//         }
+//     } catch (err){
+//         console.log('automated_build_function error: ', err)
+//     }
+//
+//     return {
+//         statusCode: 200,
+//     };
+// };
+//
+// module.exports.handler = schedule("@hourly", handler);
