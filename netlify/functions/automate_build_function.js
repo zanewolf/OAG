f22c7bb1ec3a1c16af0161170d482ea96aa2c924
zanewolf <zwolf.mlxvi@gmail.com>
2022-05-04 20:45:26 -0400

    retrying build hook

--- netlify/functions/automate_build_function.js
+++ netlify/functions/automate_build_function.js
@@ -1,27 +1,27 @@
-//
-// const { schedule } = require('@netlify/functions')
-// const fetch = require('node-fetch');
-//
-// const REBUILD_URL = process.env.GATSBY_NETLIFY_BUILD_HOOK;
-//
-// // absolute vs relative urls?
-// // build hook?
-//
-// const handler = async function(event, context) {
-//     console.log("Received event:", event)
-//
-//     try{
-//         let response = await fetch(REBUILD_URL, { method: 'POST'});
-//         if (!response.ok){
-//             throw new Error(response.statusText);
-//         }
-//     } catch (err){
-//         console.log('automated_build_function error: ', err)
-//     }
-//
-//     return {
-//         statusCode: 200,
-//     };
-// };
-//
-// module.exports.handler = schedule("@hourly", handler);
+
+const { schedule } = require('@netlify/functions')
+const fetch = require('node-fetch');
+
+const REBUILD_URL = process.env.GATSBY_NETLIFY_BUILD_HOOK;
+
+// absolute vs relative urls?
+// build hook?
+
+const handler = async function(event, context) {
+    console.log("Received event:", event)
+
+    try{
+        let response = await fetch(REBUILD_URL, { method: 'POST'});
+        if (!response.ok){
+            throw new Error(response.statusText);
+        }
+    } catch (err){
+        console.log('automated_build_function error: ', err)
+    }
+
+    return {
+        statusCode: 200,
+    };
+};
+
+module.exports.handler = schedule("@hourly", handler);
